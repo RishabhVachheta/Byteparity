fbff54117faa65b6edc9313c74b954a073bd2d60
RishabhVachheta <rishabhvachheta525@gmail.com>
2024-03-20 17:53:10 +0530

    newpattern

--- javascript/patternprog.js
+++ javascript/patternprog.js
@@ -35,22 +35,36 @@
 // }
 // console.log(string);
 
+//
+
 let n = 5;
 let string = "";
-// External loop
-for (let i = 1; i < n; i++) {
-  // printing spaces
-  for (let j = 0; j < i; j++) {
-    string += " ";
-  }
-  // printing star
-  for (let k = 0; k < 2 * (n-i) - 1; k++) {
-    string += "*";
-  }
+let pattern = "";
+for (let i = 1; i <= n; i++) {
+  string = spacing(string, 1, i);
+  string = ptr(string, i);
   string += "\n";
 }
-console.log(string);
+for (let i = n - 1; i >= 1; i--) {
+  let str = "";
+  let space = "";
+  space = spacing(space, 0, i);
+  str = ptr(str, i);
+  pattern += space + str + "\n";
+}
 
+console.log(string + pattern);
 
+function ptr(str, i) {
+  for (let k = 0; k < i * 2 - 1; k++) {
+    str += i - 1;
+  }
+  return str;
+}
 
-
+function spacing(space, type, i) {
+  for (let j = type; j < n - i; j++) {
+    space += " ";
+  }
+  return space;
+}
--- javascript/tempCodeRunnerFile.js
+++ javascript/tempCodeRunnerFile.js
@@ -1,7 +1,1 @@
- // for (let j = 0; j < i; j++) {
-  //   string += " ";
-  // }
-  // // printing star
-  // for (let k = 0; k < 2 * (n-i) - 1; k++) {
-  //   string += "*";
-  // }
+type
